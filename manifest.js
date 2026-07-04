09fefb09bc8936cb811808964809e35d9c3b3c1b
Timikoi <lmt.iyad@outlook.be>
2026-07-04 15:51:25 +0200

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,9 @@
+{
+  "name": "Chimik",
+  "short_name": "OfflineSite",
+  "start_url": "index.md",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#000000",
+  "icons": []
+}
